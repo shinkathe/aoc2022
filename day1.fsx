0b8228433aa635ca8db7eeb67553895aa729bec6
Lari Tuomisto <tuomisto.lari@gmail.com>
2022-12-01 13:46:17 +0200

    day1 - compose what can be composed

--- day1.fsx
+++ day1.fsx
@@ -1,14 +1,15 @@
+let split (separator: string) (s: string) = s.Split separator
+
 let input =
     System.IO.File.ReadAllText "inputs/day1.txt"
-    |> fun file -> file.Split "\n\n"
-    |> Array.map ((fun calorieList -> calorieList.Split "\n") >> Array.map int)
+    |> (split "\n\n" >> Array.map (split "\n" >> Array.map int))
 
-let elvesSortedByCalories = input |> Array.map (Array.sum) |> Array.sortDescending
+let elvesSortedByCalories = input |> (Array.map Array.sum >> Array.sortDescending)
 
 let elfWithMostCalories = elvesSortedByCalories |> Array.head
 
 printfn "Answer 1: %i" elfWithMostCalories
 
-let topThreeElfCalories = elvesSortedByCalories |> Array.take 3 |> Array.sum
+let topThreeElfCalories = elvesSortedByCalories |> (Array.take 3 >> Array.sum)
 
 printfn "Answer 2: %i" topThreeElfCalories
