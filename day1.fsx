4c16963925687fd9a5014d741e26523698bec902
Lari Tuomisto <tuomisto.lari@gmail.com>
2022-12-01 10:25:03 +0200

    Implement day 1

---
+++ day1.fsx
@@ -0,0 +1,15 @@
+let input =
+    System.IO.File.ReadAllText "inputs/day1.txt"
+    |> fun file -> file.Split "\n\n"
+    |> Array.map (fun calorieList -> calorieList.Split "\n")
+    |> Array.map (Array.map int)
+
+let elvesSortedByCalories = input |> Array.map (Array.sum) |> Array.sortDescending
+
+let elfWithMostCalories = elvesSortedByCalories |> Array.head
+
+printfn "Answer 1: %i" elfWithMostCalories
+
+let topThreeElfCalories = elvesSortedByCalories |> Array.take 3 |> Array.sum
+
+printfn "Answer 2: %i" topThreeElfCalories
