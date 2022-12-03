941b7b6fde2d624af23d3334d7a56591ecd6bea2
Lari Tuomisto <tuomisto.lari@gmail.com>
2022-12-03 18:12:48 +0200

    Refac more

--- day3.fsx
+++ day3.fsx
@@ -1,14 +1,14 @@
 let characterToPriority c =
     if int c >= 97 then int c - 96 else int c - 38
 
+let findIntersections over =
+    over |> (Seq.map set >> Set.intersectMany >> Seq.head)
+
 let intersectInsideRucksack rucksack =
-    rucksack
-    |> Seq.map (Seq.splitInto 2 >> Seq.map set >> Set.intersectMany >> Seq.head)
+    rucksack |> Seq.map (Seq.splitInto 2 >> findIntersections)
 
 let intersectOverRucksacks rucksacks =
-    rucksacks
-    |> Seq.chunkBySize 3
-    |> Seq.map (Seq.map set >> Set.intersectMany >> Seq.head)
+    rucksacks |> Seq.chunkBySize 3 |> Seq.map (findIntersections)
 
 let rucksack =
     System.IO.File.ReadAllLines "inputs/day3.txt"
