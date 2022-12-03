45739776f5154baaa85c291e1bff0cd4f9f107f9
Lari Tuomisto <tuomisto.lari@gmail.com>
2022-12-03 14:56:18 +0200

    Day3 refac

--- day3.fsx
+++ day3.fsx
@@ -1,17 +1,19 @@
-let getPriority c =
+let characterToPriority c =
     if int c >= 97 then int c - 96 else int c - 38
 
 let findIntersections = Array.map Set.ofArray >> Set.intersectMany >> Set.toArray
 
-let intersectByChunk (chunkSize: int) (e: int[][]) =
-    e |> Array.chunkBySize chunkSize |> Array.map (findIntersections >> Array.head)
+let intersectOverRucksacks (rucksackCount: int) (e: int[][]) =
+    e
+    |> Array.chunkBySize rucksackCount
+    |> Array.map (findIntersections >> Array.head)
 
-let existsTwice =
+let intersectInsideRucksack =
     Array.map (Array.splitInto 2) >> Array.map (findIntersections >> Array.head)
 
 let rucksack =
     System.IO.File.ReadAllLines "inputs/day3.txt"
-    |> Array.map (id >> Seq.toArray >> Array.map getPriority)
+    |> Array.map (id >> Seq.toArray >> Array.map characterToPriority)
 
-rucksack |> existsTwice |> Array.sum |> printfn "Answer 1: %A"
-rucksack |> intersectByChunk 3 |> Array.sum |> printfn "Answer 2: %i"
+rucksack |> intersectInsideRucksack |> Array.sum |> printfn "Answer 1: %A"
+rucksack |> intersectOverRucksacks 3 |> Array.sum |> printfn "Answer 2: %i"
