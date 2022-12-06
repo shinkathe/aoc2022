729b5b4bc68cd29b38ee8a594cec0197b94419ed
Lari Tuomisto <tuomisto.lari@gmail.com>
2022-12-06 13:43:39 +0200

    Day 6

--- day6.fsx
+++ day6.fsx
@@ -1,4 +1,4 @@
 let input = System.IO.File.ReadAllText "inputs/day6.txt"
-
-input |> Seq.windowed 4 |> Seq.findIndex (Array.distinct >> Array.length >> (=) 4) |> (+) 4 |> printfn "Answer 1: %i"
-input |> Seq.windowed 14 |> Seq.findIndex (Array.distinct >> Array.length >> (=) 14) |> (+) 14 |> printfn "Answer 2: %i"
+let firstDistinct len =  Seq.windowed len >> Seq.findIndex (Array.distinct >> Array.length >> (=) len) >> (+) len
+input |> firstDistinct 4 |> printfn "Answer 1: %i"
+input |> firstDistinct 14 |> printfn "Answer 2: %i"
