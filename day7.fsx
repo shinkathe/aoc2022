939d5fa2b8e9bdefef8d8e2a1c102d8a38b69b80
Lari Tuomisto <tuomisto.lari@gmail.com>
2022-12-08 02:25:50 +0200

    Fixing names

--- day7.fsx
+++ day7.fsx
@@ -4,17 +4,17 @@ let startsWith (s: string) (w: string) = s.StartsWith(w)
 let split (separator: string) (s: string) = s.Split separator
 let commands = System.IO.File.ReadAllLines "inputs/day7.txt" |> (Array.map (split " "))
 
-let runcmd curr = function
-    | [| "$"; "cd"; ".." |] -> Array.take (Array.length curr - 1) curr
+let cmd cwd = function
+    | [| "$"; "cd"; ".." |] -> Array.take (Array.length cwd - 1) cwd
     | [| "$"; "cd"; "/" |] -> [| "base" |]
-    | [| "$"; "cd"; directory |] -> Array.append curr [| directory |]
-    | [| a; b |] -> curr
+    | [| "$"; "cd"; directory |] -> Array.append cwd [| directory |]
+    | [| _; _ |] -> cwd
 
-let intermediate =
+let foldersBySize =
     commands
     |> Seq.fold
         (fun (cwd, folders) command ->
-            let currentDir = runcmd cwd command
+            let currentDir = cmd cwd command
             let newFolders =
                 match Int32.TryParse(Array.head command) with
                 | true, num -> Array.append folders [| currentDir, (num) |]
@@ -23,7 +23,7 @@ let intermediate =
         ([| "base" |], [||])
     |> snd
     |> Array.groupBy (fst)
-    |> Array.map (fun (a, b) -> (String.Join("/", a), Array.sumBy (snd) b))
+    |> Array.map (fun (path, sizes) -> (String.Join("/", path), Array.sumBy (snd) sizes))
     |> Array.fold
         (fun acc (dir, size) ->
             let recomputedAcc =
@@ -34,7 +34,7 @@ let intermediate =
             Array.concat [| [| (dir, size) |]; recomputedAcc |])
         [||]
 
-printfn "Answer 1: %i" (intermediate |> Array.sumBy (fun (_, size) -> if size <= 100000 then size else 0))
+printfn "Answer 1: %i" (foldersBySize |> Array.sumBy (fun (_, size) -> if size <= 100000 then size else 0))
 
-let limit = intermediate |> Array.find (fun (a, _) -> a = "base") |> (snd >> (+) 30000000 >> (+) -70000000)
-printfn "Answer 2: %A" (intermediate |> Array.where (snd >> (<=) limit) |> Array.sortBy (snd) |> Array.head)
+let limit = foldersBySize |> Array.find (fun (a, _) -> a = "base") |> (snd >> (+) 30000000 >> (+) -70000000)
+printfn "Answer 2: %A" (foldersBySize |> Array.where (snd >> (<=) limit) |> Array.sortBy (snd) |> Array.head)
