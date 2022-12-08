c55d2e3c989d46502a6944104262263338b4682f
Lari Tuomisto <tuomisto.lari@gmail.com>
2022-12-08 02:20:33 +0200

    Finally... Day 7

--- day7.fsx
+++ day7.fsx
@@ -4,42 +4,47 @@ let startsWith (s: string) (w: string) = s.StartsWith(w)
 let split (separator: string) (s: string) = s.Split separator
 
 let commands =
-    System.IO.File.ReadAllLines "inputs/day7.txt"
-    |> (Array.map (split " "))
-    |> Seq.toArray
+    System.IO.File.ReadAllLines "inputs/day7.txt" |> (Array.map (split " "))
 
 let runcmd curr =
     function
     | [| "$"; "cd"; ".." |] -> Array.take (Array.length curr - 1) curr
     | [| "$"; "cd"; "/" |] -> [| "base" |]
     | [| "$"; "cd"; directory |] -> Array.append curr [| directory |]
-    | [| _; _ |] -> curr
+    | [| a; b |] -> curr
 
-commands
-|> Seq.fold
-    (fun (cwd, folders) command ->
-        let newFolders =
-            match Int32.TryParse(Array.head command) with
-            | true, num -> Array.append folders [| cwd, (num) |]
-            | _ -> folders
+let intermediate =
+    commands
+    |> Seq.fold
+        (fun (cwd, folders) command ->
+            let currentDir = runcmd cwd command
+            let newFolders =
+                match Int32.TryParse(Array.head command) with
+                | true, num -> Array.append folders [| currentDir, (num) |]
+                | _ -> Array.append folders [| currentDir, (0) |]
+            (currentDir, newFolders))
+        ([| "base" |], [||])
+    |> snd
 
-        (runcmd cwd command, newFolders))
-    ([| "base" |], [||])
-|> snd
-|> Array.groupBy (fst)
-|> Array.map (fun (a, b) -> (String.Join("/", a), Array.sumBy (snd) b))
-|> Array.fold
-    (fun acc (dir, size) ->
-        let recomputedAcc =
-            acc
-            |> Array.map (fun (subDirectory: string, subDirSize) ->
-                subDirectory,
-                if startsWith dir subDirectory then
-                    subDirSize + size
-                else
-                    subDirSize)
+let intermediate2 =
+    intermediate
+    |> Array.groupBy (fst)
+    |> Array.map (fun (a, b) -> (String.Join("/", a), Array.sumBy (snd) b))
+    |> Array.fold
+        (fun acc (dir, size) ->
+            let recomputedAcc =
+                acc
+                |> Array.map (fun (subDirectory: string, subDirSize) ->
+                    subDirectory,
+                    if startsWith dir subDirectory then subDirSize + size else subDirSize)
 
-        Array.concat [| [| (dir, size) |]; recomputedAcc |])
-    [||]
-|> Array.sumBy (fun (_, size) -> if size <= 100000 then size else 0)
-|> printfn "%A"
+            Array.concat [| [| (dir, size) |]; recomputedAcc |])
+        [||]
+
+printfn "Answer 1: %i" (intermediate2 |> Array.sumBy (fun (_, size) -> if size <= 100000 then size else 0))
+
+let limit =
+    intermediate2 |> Array.find (fun (a, _) -> a = "base") |> (snd >> (+) 30000000 >> (+) -70000000)
+
+intermediate2
+|> Array.where (snd >> (<=) limit) |> Array.sortBy (snd) |> Array.head |> printfn "Answer 2: %A"
