6e853b981dd5b23ce8b5026da8fd6d349da63665
Lari Tuomisto <tuomisto.lari@gmail.com>
2022-12-08 02:44:33 +0200

    More golf

--- day7.fsx
+++ day7.fsx
@@ -21,8 +21,7 @@ let foldersBySize =
     |> Array.map (fun (path, sizes) -> (String.Join("/", path), Array.sumBy (snd) sizes))
     |> Array.fold (fun acc (dir, size) ->
             let sizesCombined = acc |> Array.map (fun (subDir: string, subDirSize) -> subDir, if dir.StartsWith(subDir) then subDirSize + size else subDirSize)
-            Array.concat [| [| (dir, size) |]; sizesCombined |])
-        [||]
+            Array.concat [| [| (dir, size) |]; sizesCombined |]) [||]
 
 printfn "Answer 1: %i" (foldersBySize |> Array.sumBy (fun (_, size) -> if size <= 100000 then size else 0))
 
