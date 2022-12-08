15a320f4049d63b0c79f34e759c6c8d19621467a
Lari Tuomisto <tuomisto.lari@gmail.com>
2022-12-08 02:23:13 +0200

    golf formatting

--- day7.fsx
+++ day7.fsx
@@ -2,12 +2,9 @@ open System
 
 let startsWith (s: string) (w: string) = s.StartsWith(w)
 let split (separator: string) (s: string) = s.Split separator
+let commands = System.IO.File.ReadAllLines "inputs/day7.txt" |> (Array.map (split " "))
 
-let commands =
-    System.IO.File.ReadAllLines "inputs/day7.txt" |> (Array.map (split " "))
-
-let runcmd curr =
-    function
+let runcmd curr = function
     | [| "$"; "cd"; ".." |] -> Array.take (Array.length curr - 1) curr
     | [| "$"; "cd"; "/" |] -> [| "base" |]
     | [| "$"; "cd"; directory |] -> Array.append curr [| directory |]
@@ -25,9 +22,6 @@ let intermediate =
             (currentDir, newFolders))
         ([| "base" |], [||])
     |> snd
-
-let intermediate2 =
-    intermediate
     |> Array.groupBy (fst)
     |> Array.map (fun (a, b) -> (String.Join("/", a), Array.sumBy (snd) b))
     |> Array.fold
@@ -37,14 +31,10 @@ let intermediate2 =
                 |> Array.map (fun (subDirectory: string, subDirSize) ->
                     subDirectory,
                     if startsWith dir subDirectory then subDirSize + size else subDirSize)
-
             Array.concat [| [| (dir, size) |]; recomputedAcc |])
         [||]
 
-printfn "Answer 1: %i" (intermediate2 |> Array.sumBy (fun (_, size) -> if size <= 100000 then size else 0))
+printfn "Answer 1: %i" (intermediate |> Array.sumBy (fun (_, size) -> if size <= 100000 then size else 0))
 
-let limit =
-    intermediate2 |> Array.find (fun (a, _) -> a = "base") |> (snd >> (+) 30000000 >> (+) -70000000)
-
-intermediate2
-|> Array.where (snd >> (<=) limit) |> Array.sortBy (snd) |> Array.head |> printfn "Answer 2: %A"
+let limit = intermediate |> Array.find (fun (a, _) -> a = "base") |> (snd >> (+) 30000000 >> (+) -70000000)
+printfn "Answer 2: %A" (intermediate |> Array.where (snd >> (<=) limit) |> Array.sortBy (snd) |> Array.head)
