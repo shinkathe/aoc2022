634a9ffae30674cca15407d8e6ad9a613217420b
Lari Tuomisto <tuomisto.lari@gmail.com>
2022-12-14 02:50:42 +0200

    Lol

--- day9.fsx
+++ day9.fsx
@@ -10,7 +10,11 @@ let computeMovement (x1, y1) (x2, y2) =
     (distanceClamped distance dx), (distanceClamped distance dy)
 
 let tail = Seq.scan(fun from target -> add from (computeMovement from target)) (0, 0)
+let kyuubi = (tail >> tail >> tail >> tail >> tail >> tail >> tail >> tail >> tail)
 let ops = System.IO.File.ReadAllLines "inputs/day9_r.txt" |> Seq.map ((fun ln -> ln.Split " ") >> fun ([|dir; amount|]) -> (dir, int amount)) |> Seq.collect (fun (dir, amount) -> [|1..amount|] |> Seq.map (fun _ -> move dir))
+let printAnswer str fn = ops |> Seq.scan (add) (0, 0) |> fn |> Seq.distinct |> Seq.length |> printfn str
 
-ops |> Seq.scan (add) (0, 0) |> tail |> Seq.distinct |> Seq.length |> printfn "Answer1: %A"
-ops |> Seq.scan (add) (0, 0) |> tail |> tail |> tail |> tail |> tail |> tail |> tail |> tail |> tail |> Seq.distinct |> Seq.length |> printfn "Answer2: %A"
+printAnswer "Answer 1 %A" tail
+printAnswer "Answer 2 %A" kyuubi
+
+// ops |> Seq.scan (add) (0, 0) |> kyuubi |> Seq.distinct |> Seq.length |> printfn "Answer2: %A"
