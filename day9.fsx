2dced2575c695a02e5b04a5d3efee2e9d18fb726
Lari Tuomisto <tuomisto.lari@gmail.com>
2022-12-14 03:06:36 +0200

    More golfffffff

--- day9.fsx
+++ day9.fsx
@@ -3,11 +3,10 @@ open System.Numerics
 
 let move = function | 'D' -> (0, -1) | 'L' -> (-1, 0) | 'R' -> (1, 0) | 'U' -> (0, 1)
 let add (fromX, fromY) (addX, addY) = (fromX + addX, fromY + addY)
-let distanceClamped distance delta  = if (distance < 2.0f) then 0 else Math.Clamp(delta, -1, 1)
+let clampDeltaByDistance distance delta  = if (distance < 2.0f) then 0 else Math.Clamp(delta, -1, 1)
 let computeMovement (x1, y1) (x2, y2) =
-    let dx, dy = x2 - x1, y2 - y1
     let distance = Vector2.Distance(Vector2((float32 x1), (float32 y1)), Vector2((float32 x2), (float32 y2)))
-    (distanceClamped distance dx), (distanceClamped distance dy)
+    clampDeltaByDistance distance x2 - x1, clampDeltaByDistance distance y2 - y1
 
 let head = Seq.scan (add) (0, 0)
 let tail = Seq.scan(fun from target -> add from (computeMovement from target)) (0, 0)
