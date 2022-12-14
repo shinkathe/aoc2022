f4a93af7d867fc9a716cd774201c87b550867677
Lari Tuomisto <tuomisto.lari@gmail.com>
2022-12-14 19:53:42 +0200

    Day 10

---
+++ day10.fsx
@@ -0,0 +1,17 @@
+open System;
+let concat (s: string[]) = String.Join("", s)
+let parseOrDefault (defaultTo: int) (str: string) = match System.Int32.TryParse str with | true, int -> int | _ -> defaultTo
+
+let instructions = System.IO.File.ReadAllLines "inputs/day10_r.txt" |> (Array.map (fun s -> s.Split " " |> (Array.last >> parseOrDefault 0)))
+
+let cycles =
+    instructions
+     |> Array.fold (fun acc instruction ->
+        let index, register = Array.last acc
+        match instruction with
+            | 0 -> Array.concat [|acc; [|(index + 1, register)|]|]
+            | x -> Array.concat [|acc; [|(index + 1, register)|]; [|(index + 2), register + x|]|]
+        ) [|(1, 1)|]
+
+[|20;60;100;140;180;220|] |> Array.sumBy (fun i -> (cycles[i-1] |> snd) * i) |> printfn "Answer 1: %A"
+cycles |> Array.chunkBySize 40 |> Array.map (Array.mapi (fun col (_, x) -> if (x=col-1 || x=col || x=col+1) then "#" else ".")) |> Array.map (concat >> printfn "%A")
