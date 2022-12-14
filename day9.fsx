de2db0ab3c6cc857b2241e6c0a0da64b79108b3a
Lari Tuomisto <tuomisto.lari@gmail.com>
2022-12-14 03:09:47 +0200

    Mistakes were made

--- day9.fsx
+++ day9.fsx
@@ -6,10 +6,10 @@ let add (fromX, fromY) (addX, addY) = (fromX + addX, fromY + addY)
 let clampDeltaByDistance distance delta  = if (distance < 2.0f) then 0 else Math.Clamp(delta, -1, 1)
 let computeMovement (x1, y1) (x2, y2) =
     let distance = Vector2.Distance(Vector2((float32 x1), (float32 y1)), Vector2((float32 x2), (float32 y2)))
-    clampDeltaByDistance distance x2 - x1, clampDeltaByDistance distance y2 - y1
+    clampDeltaByDistance distance (x2 - x1), clampDeltaByDistance distance (y2 - y1)
 
 let head = Seq.scan (add) (0, 0)
-let tail = Seq.scan(fun from target -> add from (computeMovement from target)) (0, 0)
+let tail = Seq.scan (fun from target -> add from (computeMovement from target)) (0, 0)
 
 let ops = System.IO.File.ReadLines "inputs/day9_r.txt" |> Seq.map (fun s -> s[0], int s[2..]) |> Seq.collect (fun (dir, amount) -> [|1..amount|] |> Seq.map (fun _ -> move dir))
 
